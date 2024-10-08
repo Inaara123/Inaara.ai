5891eea717dc27a160e2a9cfd3cc8ae4c3aace48
Inaara123 <tech@inaara.ai>
2024-10-09 00:43:57 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,56 +1,15 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
 import { FaFacebookF,FaInstagram,FaTwitter } from 'react-icons/fa';
 const Footer = () => {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
-
-  const handleMouseEnter = (index) => {
-    setHoveredIndex(index);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredIndex(null);
-  };
-
-  const text = 'Inaara.ai';
-
   return (
-    <div className="h-[100vh] bg-black">
-      <div className="h-full w-full flex items-center justify-center">
-      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-9xl text-white author-medium flex space-x-2 cursor-pointer">
-          {text.split('').map((char, index) => (
-            <motion.span
-              key={index}
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={handleMouseLeave}
-              initial={{ opacity: 0.5 }}
-              animate={{
-                opacity:
-                  hoveredIndex === index ||
-                  hoveredIndex === index - 1 ||
-                  hoveredIndex === index + 1
-                    ? 1
-                    : 0.5,
-                scale:
-                  hoveredIndex === index ||
-                  hoveredIndex === index - 1 ||
-                  hoveredIndex === index + 1
-                    ? 1.2
-                    : 1,
-                transition: { duration: 0.3 },
-              }}
-              className="glow"
-            >
-              {char}
-            </motion.span>
-          ))}
-        </h1>
-      </div>
+    
       <footer className="bg-gray-900 text-white py-8">
       <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             {/* Column 1: About */}
             <div>
+            <h1 className="text-2xl font-bold mb-4 text-white">
+              INAARA AI SOLUTIONS PRIVATE LIMITED
+            </h1>
               <h2 className="text-xl font-bold mb-4">About Us</h2>
               <p>
               <a href="https://inaara.ai/aboutus" className="text-gray-400 hover:text-white">
@@ -126,7 +85,6 @@ const Footer = () => {
             </div>
             </div>
       </footer>
-    </div>
     
   );
 };
